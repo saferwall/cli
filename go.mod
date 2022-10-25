5b4eef639dc95fc5aeda8d9594f146f21762659e
LordNoteworthy <lord.noteworthy@protonmail.com>
2022-10-25 17:31:31 +1100

    move utility functions to internal/

--- go.mod
+++ go.mod
@@ -1,10 +1,28 @@
 module github.com/saferwall/saferwall-cli
 
-go 1.16
+go 1.17
 
 require (
-	github.com/atotto/clipboard v0.1.4
-	github.com/aws/aws-sdk-go v1.38.55
+	github.com/Masterminds/sprig v2.22.0+incompatible
+	github.com/gammazero/workerpool v1.1.2
 	github.com/joho/godotenv v1.3.0
-	github.com/spf13/cobra v1.1.3
+	github.com/spf13/cobra v1.2.1
+	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4
+	gopkg.in/yaml.v2 v2.4.0
+)
+
+require (
+	github.com/Masterminds/goutils v1.1.1 // indirect
+	github.com/Masterminds/semver v1.5.0 // indirect
+	github.com/gammazero/deque v0.1.0 // indirect
+	github.com/google/uuid v1.1.2 // indirect
+	github.com/huandu/xstrings v1.3.2 // indirect
+	github.com/imdario/mergo v0.3.12 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/kr/pretty v0.2.0 // indirect
+	github.com/mitchellh/copystructure v1.2.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.2 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 )
---
+++ internal/util/utils.go
@@ -0,0 +1,95 @@
+// Copyright 2022 Saferwall. All rights reserved.
+// Use of this source code is governed by Apache v2 license
+// license that can be found in the LICENSE file.
+
+package util
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+func check(e error) {
+	if e != nil {
+		log.Fatal(e)
+	}
+}
+
+func exitErrorf(msg string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, msg+"\n", args...)
+	os.Exit(1)
+}
+
+func difference(a, b []string) []string {
+	mb := make(map[string]struct{}, len(b))
+	for _, x := range b {
+		mb[x] = struct{}{}
+	}
+	var diff []string
+	for _, x := range a {
+		if _, found := mb[x]; !found {
+			diff = append(diff, x)
+		}
+	}
+	return diff
+}
+
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
+
+// GetSha256 returns SHA256 hash.
+func GetSha256(b []byte) string {
+	h := sha256.New()
+	h.Write(b)
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+// WriteBytesFile write Bytes to a File.
+func WriteBytesFile(filename string, r io.Reader) (int, error) {
+
+	// Open a new file for writing only
+	file, err := os.OpenFile(
+		filename,
+		os.O_WRONLY|os.O_TRUNC|os.O_CREATE,
+		0666,
+	)
+	if err != nil {
+		return 0, err
+	}
+	defer file.Close()
+
+	// Read for the reader bytes to file
+	b, err := ioutil.ReadAll(r)
+	if err != nil {
+		return 0, err
+	}
+
+	// Write bytes to disk
+	bytesWritten, err := file.Write(b)
+	if err != nil {
+		return 0, err
+	}
+
+	return bytesWritten, nil
+}
+
+// Exists reports whether the named file or directory exists.
+func Exists(name string) bool {
+	if _, err := os.Stat(name); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
+}
